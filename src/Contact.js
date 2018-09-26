f8bdba54c9b9a75e6e06ea80873d3f982cae3da1
Stephen Cooke <steve_cooke_20@hotmail.com>
2018-09-26 20:58:32 +0100

    Contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './stylesheets/Contact.css';
 
-const Contact = () => (
-  <div id="contact-form">
-    <h3 className="title">Contact</h3>
-    <form name="contact" id="contact-form" method="POST" netlify>
 
-      <input type="text" id="name" name="name" value="Name" />
-      <input type="email" id="Email" name="email" value="Email" />
-      <textarea type="text" id="addit" name="info" value="Message.." />
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Name: 'Name',
+      Email: 'Email',
+      Message: 'Message..',
+    };
 
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-export default Contact;
+  handleChange(event) {
+    this.setState({ Name: event.target.value });
+  }
+
+  render() {
+    const { Name, Email, Message } = this.state;
+    return (
+
+      <div id="contact-form">
+        <h3 className="title">Contact</h3>
+        <form name="contact" id="contact-form" method="POST" netlify="true">
+
+          <input type="text" id="Name" name="name" value={Name} onChange={this.handleChange} />
+          <input type="email" id="Email" name="email" value={Email} onChange={this.handleChange} />
+          <textarea type="text" id="addit" name="info" value={Message} onChange={this.handleChange} />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}

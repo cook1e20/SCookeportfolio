44c860b04cc1c5ca59a67a18ff3789e4e727f31e
Stephen Cooke <steve_cooke_20@hotmail.com>
2018-09-26 21:15:28 +0100

    Contactalmostdone

--- src/Contact.js
+++ src/Contact.js
@@ -15,6 +15,10 @@ export default class Contact extends Component {
   }
 
   handleChange(event) {
+    const { name } = event.target;
+    if (name === event.target.value.slice(0, name.length)) {
+      this.setState({ [event.target.name]: '' });
+    }
     this.setState({ [event.target.name]: event.target.value });
   }
 
@@ -26,9 +30,9 @@ export default class Contact extends Component {
         <h3 className="title">Contact</h3>
         <form name="contact" id="contact-form" method="POST" netlify="true">
 
-          <input type="text" id="Name" name="name" value={Name} onChange={this.handleChange} />
-          <input type="email" id="Email" name="email" value={Email} onChange={this.handleChange} />
-          <textarea type="text" id="addit" name="info" value={Message} onChange={this.handleChange} />
+          <input type="text" id="Name" name="Name" value={Name} onChange={this.handleChange} />
+          <input type="email" id="Email" name="Email" value={Email} onChange={this.handleChange} />
+          <textarea type="text" id="addit" name="Message" value={Message} onChange={this.handleChange} />
 
           <button type="submit">Submit</button>
         </form>

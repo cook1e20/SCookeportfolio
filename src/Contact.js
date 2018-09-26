7dbff24a53fc3255aae964680c578bd7503f294f
Stephen Cooke <steve_cooke_20@hotmail.com>
2018-09-26 20:08:14 +0100

    Contact css

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './stylesheets/Contact.css';
 
 const Contact = () => (
-  <div>
+  <div id="contact-form">
     <h3 className="title">Contact</h3>
     <form name="contact" id="contact-form" method="POST" netlify>
 
-      <input type="text" id="name" name="name" value="name" />
+      <input type="text" id="name" name="name" value="Name" />
       <input type="email" id="Email" name="email" value="Email" />
       <textarea type="text" id="addit" name="info" value="Message.." />
 
